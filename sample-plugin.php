647853690685d16f13431f4892ee27bf0a810c85
T. Kameda <kame@toiee.jp>
2021-03-22 16:53:48 +0900

    ver0.1.1 をリリース

--- sample-plugin.php
+++ sample-plugin.php
@@ -7,7 +7,7 @@
  * Author URI:      https://toiee.jp
  * Text Domain:     sample-plugin
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         0.1.1
  *
  * @package         Sample_Plugin
  */
